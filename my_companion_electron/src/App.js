5d4ec9431ca62bd23a49db90e4288e7e26046c62
Ieuan Jones <JonesIN@cardiff.ac.uk>
2019-02-13 20:21:43 +0000

    Add initial Electron app running create-react-app; Add roslib JS dependency; Add example code to publish message to ROS topic

---
+++ my_companion_electron/src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+const ROSLIB = require('roslib');
+
+const ros = new ROSLIB.Ros({
+      url: 'ws://localhost:9090'
+});
+
+const topic = new ROSLIB.Topic({
+      ros : ros,
+      name : 'example_topic',
+      messageType: 'std_msgs/String'
+});
+
+ros.on('connection', () => {
+      console.log('Connected to websocket server.');
+
+      const message = new ROSLIB.Message({
+          data: "Hello world"
+      });
+
+      topic.publish(message);
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const message = new ROSLIB.Message({
+      data: event.target.value
+    });
+    topic.publish(message);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+	  <form>
+           <label>
+            Name:
+            <input type="text" onChange= {this.handleChange} />
+            </label>
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
