9face823701885694824cccfa6df30d0ad88d57e
Christopher Cardwell <CardwellCJ@cardiff.ac.uk>
2019-02-24 18:25:56 +0000

    Integrated the audio visualiser that another team member found into the existing UI.

--- my_companion_electron/src/App.js
+++ my_companion_electron/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import AudioAnalyser from './AudioAnalyser';
 import './App.css';
 const ROSLIB = require('roslib');
 
@@ -27,6 +27,32 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      audio: null
+    };
+    this.toggleMicrophone = this.toggleMicrophone.bind(this);
+
+  }
+  
+  async getMicrophone() {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    this.setState({ audio });
+  }
+  
+  stopMicrophone() {
+    this.state.audio.getTracks().forEach(track => track.stop());
+    this.setState({ audio: null });
+  }
+  
+  toggleMicrophone() {
+    if (this.state.audio) {
+      this.stopMicrophone();
+    } else {
+      this.getMicrophone();
+    }
   }
 
   handleChange(event) {
@@ -39,7 +65,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div className="controls">
+          <button onClick={this.toggleMicrophone}>
+            {this.state.audio ? 'Stop Microphone' : 'Enable Microphone Input'}
+          </button>
+        </div>
+        {this.state.audio ? <AudioAnalyser audio={this.state.audio} /> : ''}
       </div>
     );
   }
