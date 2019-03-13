776c9c788f829ea454f6e023a953d380a06524b3
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-03-13 16:27:25 +0000

    Added inline comments to the script that details what each line does

--- my_companion/scripts/temperature.py
+++ my_companion/scripts/temperature.py
@@ -4,9 +4,11 @@ import glob
 import time
 import datetime
 
+# Initialize the GPIO Pins
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
+# Finds the correct device file that holds the temperature data
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
