65eea3bdbdb8cc68dd13dde5d5f5de811dd0fce7
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-03-13 16:29:41 +0000

    Created a function that opens the temperature data file and returns the text

--- my_companion/scripts/temperature.py
+++ my_companion/scripts/temperature.py
@@ -12,3 +12,10 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+# Function that reads the sensor data
+def read_temp_raw():
+    f = open(device_file, 'r') # opens the temperature data file
+    lines = f.readlines() # returns the text
+    f.close
+    return lines
