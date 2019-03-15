91d72fb80d8f835cad97ef28152488d727619eac
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-03-15 13:47:31 +0000

    set a variable pinpir to 17 to hold the GPIO pin identity. Also set GPIO pin as input

--- my_companion/scripts/motion.py
+++ my_companion/scripts/motion.py
@@ -5,3 +5,11 @@ import time
 # Set the GPIO naming convention
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
+# Set a variable to hold the GPIO pin identity
+pinpir = 17
+
+print("PIR Motion Module Test (CTRL-C to Exit)")
+
+# Set pin as input
+GPIO.setup(pinpir, GPIO.IN)
