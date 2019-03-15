e3e54a5e435caece1a338f98995e97ee9ea02dba
Ieuan Jones <JonesIN@cardiff.ac.uk>
2019-03-15 15:55:29 +0000

    add import statement for DateTimeFormatter; add bracket in Temperature entity

--- my_companion_server/src/main/java/com/mycompanion/mycompanion/service/TemperatureServiceImpl.java
+++ my_companion_server/src/main/java/com/mycompanion/mycompanion/service/TemperatureServiceImpl.java
@@ -9,6 +9,8 @@ import com.mycompanion.mycompanion.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.format.DateTimeFormatter;
+
 @Service
 public class TemperatureServiceImpl implements TemperatureService {
 
