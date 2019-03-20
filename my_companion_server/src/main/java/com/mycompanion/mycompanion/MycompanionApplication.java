5c452ff088a9c394836ab2fcfa18adad3f1f2790
Ieuan Jones <vagrant@vagrant.com>
2019-03-20 21:34:59 +0000

    configure project for Azure in build.gradle and application.properties to produce WAR file and to publish to Azure; add SpringBootServletInitializer configuration to produce WAR file; add index.html

--- my_companion_server/src/main/java/com/mycompanion/mycompanion/MycompanionApplication.java
+++ my_companion_server/src/main/java/com/mycompanion/mycompanion/MycompanionApplication.java
@@ -2,12 +2,27 @@ package com.mycompanion.mycompanion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class MycompanionApplication {
+@RestController
+public class MycompanionApplication extends SpringBootServletInitializer {
+
+	@Override
+   	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      		return application.sources(MycompanionApplication.class);
+   	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycompanionApplication.class, args);
 	}
+
+	@RequestMapping(value = "/")
+   	public String hello() {
+      		return "Hello World from Tomcat";
+   	}
 
 }
