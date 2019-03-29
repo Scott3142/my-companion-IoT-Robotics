da3bbc7f6ea9f6cb81b353b938977041fa22a68d
Daniel Abbasi <AbbasiD@cardiff.ac.uk>
2019-03-29 14:42:31 +0000

    modified temperature script to send temperature readings to server using request.post

--- my_companion/scripts/temperature.py
+++ my_companion/scripts/temperature.py
@@ -1,69 +1,56 @@
-#!/usr/bin/env python
 import os
 import glob
 import time
 import datetime
-import rospy
-from my_companion.msg import Temperature
+import requests
 
-# Initialize the GPIO Pins
+class Temp:
+        uuid = 0
+        sensorName = ""
+        temperature = 0
+        humidity = 0
+        timestamp = ""
+
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-# Finds the correct device file that holds the temperature data
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-pub = rospy.Publisher('/temperature', Temperature, queue_size=10)
-
-
-# Function that reads the sensor data
 def read_temp_raw():
-    f = open(device_file, 'r') # opens the temperature data file
-    lines = f.readlines() # returns the text
-    f.close
-    return lines
+	f = open(device_file, 'r')
+	lines = f.readlines()
+	f.close()
+	return lines
 
-# Convert the value of the sensor into a temperature
 def read_temp():
-    lines = read_temp_raw() # read the temperature device file
+	lines = read_temp_raw()
 
-    # While the first line does not contain 'YES' then wait 0.2s and then read the device file again
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw
-    # Look for the position of the '=' in the second line of the device file
-    equals_pos = lines[1].find('t=')
+	while lines[0].strip()[-3:] != 'YES':
+		time.sleep(0.2)
+		lines = read_temp_raw()
 
-    # If the '=' is found then convert the rest of the line into degrees Celsius and take the current time stamp
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        time = datetime.datetime.now()
-
-        msg = Temperature()
-        msg.uuid = 1
-        msg.sensorName = "Kitchen"
-        msg.temperature = temp_c
-        msg.humidity = 0
-        msg.timestamp = time.strftime("%Y-%m-%dT%H:%M:%S.%f")
-
-        pub.publish(msg)
-
-        return temp_c, time.strftime("%Y-%m-%dT%H:%M:%S.%f") # Format date time into readable form
-
-# Print temperature until program stops
+	equals_pos = lines[1].find('t=')
 
 
-def temp_talker():
-    rospy.init_node('temperature_node', anonymous=True)
-    r = rospy.Rate(10) #10hz
-    rospy.spin()
+	if equals_pos != -1:
+		temp_string = lines[1][equals_pos+2:]
+		temp_c = float(temp_string) / 1000.0
+		time = datetime.datetime.now()
 
-    while True:
-        rospy.loginfo(read_temp())
-        time.sleep(5)
+		temp = Temp()
 
-if __name__ == '__main__':
-    temp_talker()
+		temp.uuid = 1
+		temp.sensorName = "Kitchen"
+		temp.temperature = temp_c
+		temp.humidity = 0
+		temp.timestamp = time.strftime("%Y-%m-%dT%H:%M:%S.%f")
+		requests.post('http://10.72.97.47:8080/api/temperatures/', json=temp.__dict__)
+
+		return temp_c, time.strftime("%Y-%m-%dT%H:%M:%S.%f")
+	
+
+while True:
+	print(read_temp())
+	time.sleep(1)
