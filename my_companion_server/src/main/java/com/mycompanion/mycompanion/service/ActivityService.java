033096cddbcbfd02e0d6f3ae481cafacfa550193
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-04-01 20:48:45 +0100

    created an activity service interface with a method save activity which will be defined in the implementation of this class

---
+++ my_companion_server/src/main/java/com/mycompanion/mycompanion/service/ActivityService.java
@@ -0,0 +1,8 @@
+package com.mycompanion.mycompanion.service;
+
+
+import com.mycompanion.mycompanion.dto.ActivityDTO;
+
+public interface ActivityService {
+    ActivityDTO saveActivity(ActivityDTO newActivity);
+}
