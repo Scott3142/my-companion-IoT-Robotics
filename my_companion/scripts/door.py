9ccf64bbd04b7feb132477dac64614f0695b0f79
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-04-01 12:24:11 +0100

    Created door python script that detects a change in the door sensor state by remembering its state from the previous loop iteration and comparing it to that of the current iteration. If there is a change it will print to the terminal

---
+++ my_companion/scripts/door.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+# Set Broadcom mode so we can address GPIO pins by number.
+GPIO.setmode(GPIO.BCM)
+# This is the GPIO pin number we have one of the door sensor
+# wires attached to, the other should be attached to a ground
+DOOR_SENSOR_PIN = 18
+# Initially we don't know if the door sensor is open or closed
+isOpen = None
+oldIsOpen = None
+
+# Set up the door sensor pin.
+GPIO.setup(DOOR_SENSOR_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+while True:
+    oldIsOpen = isOpen
+    isOpen = GPIO.input(DOOR_SENSOR_PIN)
+    if (isOpen and (isOpen != oldIsOpen)):
+        print "Space is unoccupied!"
+    elif (isOpen != oldIsOpen):
+        print "Space is occupied!"
+    time.sleep(0.1)
