90da3b5e26e80322bb9f45687ff2b54a08b19ae7
Vagrant <vagrant>
2019-04-03 11:43:04 +0100

    Updated pepper mood behaviour to be a timeline to avoid crashing since it cannot send to server. Added trigger function by tapping peppers head and when a person comes near the robot. Also goes to the start of the function when user touches the back of peppers head

--- Pepper_Mood_Check/translations/translation_en_US.ts
+++ Pepper_Mood_Check/translations/translation_en_US.ts
@@ -2,6 +2,15 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
+        <name>behavior_1/behavior.xar:/Main/behavior_layer1/action/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>OK I'm ready</source>
+            <comment>Text</comment>
+            <translation type="unfinished">OK I'm ready</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>Hello</source>
@@ -9,10 +18,9 @@
             <translation type="vanished">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>OK I'm ready</source>
             <comment>Text</comment>
-            <translation type="unfinished">OK I'm ready</translation>
+            <translation type="obsolete">OK I'm ready</translation>
         </message>
     </context>
 </TS>
